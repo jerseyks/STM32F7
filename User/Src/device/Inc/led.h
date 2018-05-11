b090e1ffcfe68c570cba3e20321a0ca338308715
aliasfly <zhangzexiangks@126.com>
2018-05-11 08:59:36 +0800

    move led.cpp content to led.h

--- User/Src/device/Inc/led.h
+++ User/Src/device/Inc/led.h
@@ -36,6 +36,123 @@ typedef enum
 #define STM32_LED3_PIN                         GPIO_PIN_7
 
 
+const uint32_t STM32_GPIO_PIN[ledn] = {STM32_LED1_PIN,STM32_LED2_PIN,STM32_LED3_PIN};
+
+#ifdef __cplusplus
+class Led
+{
+    public:
+        void STM32_LED_Init(STM32_LED_TypeDef led)
+        {
+            GPIO_InitTypeDef  gpio_init_structure;
+            GPIO_TypeDef*     gpio_led;
+
+            if ((led == STM32_LED1) || (led == STM32_LED2) || (led == STM32_LED3))
+            {
+                if (led == STM32_LED1)
+                {
+                    gpio_led = STM32_LED1_GPIO_PORT;
+                    STM32_LED1_GPIO_CLK_ENABLE();
+                }
+                else 
+                    if(led == STM32_LED2)
+                    {
+                        gpio_led = STM32_LED2_GPIO_PORT;
+                        STM32_LED2_GPIO_CLK_ENABLE();
+                    }
+                    else
+                    {
+                        gpio_led = STM32_LED3_GPIO_PORT;
+                        STM32_LED3_GPIO_CLK_ENABLE();    
+                    }
+
+                gpio_init_structure.Pin = STM32_GPIO_PIN[led];
+                gpio_init_structure.Mode = GPIO_MODE_OUTPUT_PP;
+                gpio_init_structure.Pull = GPIO_PULLUP;
+                gpio_init_structure.Speed = GPIO_SPEED_HIGH;
+            
+                HAL_GPIO_Init(gpio_led, &gpio_init_structure);
+                
+                HAL_GPIO_WritePin(gpio_led, STM32_GPIO_PIN[led], GPIO_PIN_SET);
+            }
+        };
+        void STM32_LED_On(STM32_LED_TypeDef led)
+        {
+            GPIO_TypeDef*     gpio_led;
+
+            if ((led == STM32_LED1) || (led == STM32_LED2) || (led == STM32_LED3))
+            {
+                if (led == STM32_LED1)
+                {
+                    gpio_led = STM32_LED1_GPIO_PORT;
+                }
+               else 
+                    if(led == STM32_LED2)
+                    {
+                        gpio_led = STM32_LED2_GPIO_PORT;
+                    }
+                    else
+                    {
+                        gpio_led = STM32_LED3_GPIO_PORT;   
+                    }
+                HAL_GPIO_WritePin(gpio_led, STM32_GPIO_PIN[led], GPIO_PIN_RESET);
+            }
+
+        };
+        void STM32_LED_Off(STM32_LED_TypeDef led)
+        {
+            GPIO_TypeDef*     gpio_led;
+
+            if ((led == STM32_LED1) || (led == STM32_LED2) || (led == STM32_LED3))
+            {
+                if (led == STM32_LED1)
+                {
+                    gpio_led = STM32_LED1_GPIO_PORT;
+                }
+               else 
+                    if(led == STM32_LED2)
+                    {
+                        gpio_led = STM32_LED2_GPIO_PORT;
+                    }
+                    else
+                    {
+                        gpio_led = STM32_LED3_GPIO_PORT;   
+                    }
+                HAL_GPIO_WritePin(gpio_led, STM32_GPIO_PIN[led], GPIO_PIN_SET);
+            }
+
+        };
+        void STM32_LED_Toggle(STM32_LED_TypeDef led)
+        {
+            GPIO_TypeDef*     gpio_led;
+
+            if ((led == STM32_LED1) || (led == STM32_LED2) || (led == STM32_LED3))
+            {
+                if (led == STM32_LED1)
+                {
+                    gpio_led = STM32_LED1_GPIO_PORT;
+                }
+                else 
+                    if(led == STM32_LED2)
+                    {
+                        gpio_led = STM32_LED2_GPIO_PORT;
+                    }
+                    else
+                    {
+                        gpio_led = STM32_LED3_GPIO_PORT;   
+                    }       
+                HAL_GPIO_TogglePin(gpio_led, STM32_GPIO_PIN[led]);
+            }
+        };
+    private:
+};
+
+
+Led deng1;
+
+#endif
+
+
 #ifdef __cplusplus
 }
 #endif
